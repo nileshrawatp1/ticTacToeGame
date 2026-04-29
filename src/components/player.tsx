c41e3a05c98d5dc06046fbabfee39c42aad912b0
Rawat <2397391@cognizant.com>
2026-04-29 14:44:06 +0530

    Changing the value of player added

---
+++ src/components/player.tsx
@@ -0,0 +1,35 @@
+import { useState, ChangeEvent } from "react";
+
+type playerProps = {
+    initialName: string;
+    symbol: string;
+}
+
+export default function Player({ initialName, symbol }: playerProps) {
+    const [playerName, setPlayerName] = useState<string>(initialName);
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+
+    const handleEditClick = () => {
+        setIsEditing((editing) => !editing)
+    }
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setPlayerName(e.target.value);
+    }
+
+    let playerNameTag = <span className="player-name">{playerName}</span>;
+
+    if(isEditing) {
+        playerNameTag = <input type="text" required value={playerName} onChange={handleInputChange} />
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {playerNameTag}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    )
+}
